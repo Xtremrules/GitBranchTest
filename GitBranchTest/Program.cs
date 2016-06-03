f0e5a66760c5c812cf7a46bd2420f134471fd429
Xtremrules <olelewechukwunyere@outlook.com>
2016-06-03 20:58:30 +0100

    New change

--- GitBranchTest/Program.cs
+++ GitBranchTest/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace GitBranchTest {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Welcome to my app");
+            Console.ReadKey();
         }
     }
 }
