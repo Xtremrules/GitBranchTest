82d20f4f31fbec330e32d204d46573b35de784d8
Xtremrules <olelewechukwunyere@outlook.com>
2016-06-03 21:03:32 +0100

    Removed using

--- GitBranchTest/Program.cs
+++ GitBranchTest/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitBranchTest {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Welcome to my app");
+            Console.WriteLine("Enter a value");
             var name = Console.ReadLine();
             Console.WriteLine($"Value entered is {name}");
             Console.ReadKey();
